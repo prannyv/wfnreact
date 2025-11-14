de67c8d15c6f878bfcf1f7f2db965430607adb52
Crystal Ge <crystalge@dyn172-30-75-88.wireless.uwo.pri>
2025-11-13 20:40:37 -0500

    updated crystal page

--- src/app/student/crystal-page.jsx
+++ src/app/student/crystal-page.jsx
@@ -3,6 +3,14 @@ export default function CrystalPage() {
     <div>
       <h1>Crystal</h1>
       <a href="/home">Back to Home</a>
+      <div>hi</div>
+
+      {/* CRYSTAL MUST REMEMBER THESE COMMANDS */}
+      1. git add .
+      2. git commit -m 'I updated XYZ'
+      3. git pull 
+      4. git push
     </div>
+
   );
 }
